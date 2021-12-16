3a022b38d4b19780c2002288511933641b196600
k.ryou <58724780+ryouhei-kanezawa@users.noreply.github.com>
2021-12-16 15:24:09 +0900

    211216_2

---
+++ Run-Game/Assets/script/game/BoadManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoadManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject boadObject;
+    [SerializeField]
+    private GameObject[] item;
+    [SerializeField]
+    private float interval = 2f;
+    [SerializeField]
+    private TimeStart Stop;
+
+    private float nextSpawnTime = 0;
+    private int Rnum = 0;
+    private bool active = false;
+    private void LocalInstantate()
+    {
+        GameObject obj = (GameObject)GameObject.Instantiate(boadObject);
+		if (Stop.StopMoment())
+		{
+            Rnum = Random.Range(0, item.Length);
+            GameObject opt = (GameObject)GameObject.Instantiate(item[Rnum]);
+		}
+    }
+
+    void Update()
+    {
+        if (nextSpawnTime < Time.timeSinceLevelLoad)
+        {
+            nextSpawnTime = Time.timeSinceLevelLoad + interval;
+            LocalInstantate();
+        }
+    }
+
+    public void SetActive(bool _active)
+    {
+        active = _active;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(111);
+        Destroy(collision.gameObject);
+    }
+
+	public void CleanBoadObject()
+    {
+        GameObject[] boads = GameObject.FindGameObjectsWithTag("field");
+
+        foreach(var VARIABLE in boads)
+        {
+            Destroy(VARIABLE);
+        }
+    }
+}
---
+++ Run-Game/Assets/script/game/Starr_sc.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starr_sc : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _Object;
+
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+}
